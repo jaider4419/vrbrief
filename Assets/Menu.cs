46072457dca1b6794778e8f0d6d862cf48cec3b8
jaider4419 <jaiderboateng8@gmail.com>
2025-01-23 15:42:16 +0000

    fdsafdsfdf

---
+++ Assets/Menu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playButton()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
