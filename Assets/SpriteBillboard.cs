c0e4d0f02e0da31ecc70c16b0201c4de6b6f3891
jaider4419 <jaiderboateng8@gmail.com>
2025-01-24 15:13:18 +0000

    hhhhrfdtretg

---
+++ Assets/SpriteBillboard.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SpriteBillBoard : MonoBehaviour
+{
+    [SerializeField] bool freezeXZAxis = true;
+
+    private void Update()
+    {
+        if (freezeXZAxis)
+        {
+            transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        }
+        else
+        {
+            transform.rotation = Camera.main.transform.rotation;
+        }
+    }
+}
