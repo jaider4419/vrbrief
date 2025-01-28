7c50a872a2cbf6947ad2cd8423a5a0dc16b2e665
jaider4419 <jaiderboateng8@gmail.com>
2025-01-28 10:42:08 +0000

    saver

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -9,10 +11,15 @@ public class GameController : MonoBehaviour
     public TextMeshProUGUI TimerText;
     [SerializeField] float gameTimer;
 
+    GameObject[] Moles;
+    float popupTimer = 1;
+    static public int score = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        Moles = GameObject.FindGameObjectsWithTag("Mole");
     }
 
     // Update is called once per frame
@@ -41,8 +48,34 @@ public class GameController : MonoBehaviour
         if (gameTimer == 0) 
         {
             TimerText.text = "GAME OVER!";
+            endMoleGame();
+        }
+
+        updateMoles(); 
+    }
+
+    void updateMoles()
+    {
+
+        popupTimer -= Time.deltaTime;
+
+        if (popupTimer < 0)
+        {
+            int rnd = Random.Range(0, Moles.Length);
+            var script = Moles[rnd].GetComponent<MoleJump>();
+            script.pop();
+
+            popupTimer = Random.Range(1, 3);
+           
         }
     }
 
+    void endMoleGame()
+    {
+        if (gameTimer == 0)
+        {
+            
+        }
+    }
     
 }
---
+++ Assets/SCRIPTS/MoleJump.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleJump : MonoBehaviour
+{
+
+    [Header("Popup Settings")]
+
+    [SerializeField] float popHeight = 1;
+    Vector3 startPos;
+    Vector3 popPos;
+
+    [Header("Popup Settings")]
+
+    [SerializeField] int popState = 0;
+    [SerializeField] float waitTime = 1;
+
+    private 
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        popPos = transform.position;
+        popPos.y += popHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (popState == 1)
+        {
+            transform.position = Vector3.Lerp(transform.position, popPos, 5 * Time.deltaTime);
+            if (Mathf.Abs(transform.position.y - popPos.y) < 0.1f)
+            {
+                popState = 2;
+                waitTime = Random.Range(0.5f, 2);
+            }
+        }
+
+        if (popState == 2)
+        {
+            waitTime -= Time.deltaTime;
+            if (waitTime < 0)
+            {
+                popState = 3;
+            }
+        }
+
+        if (popState == 3)
+// where the mole will go?
+        {
+            transform.position = Vector3.Lerp(transform.position, startPos, 5 * Time.deltaTime);
+
+            if (Mathf.Abs(transform.position.y - startPos.y) < 0.1f)
+            {
+                popState = 0;
+                transform.position = startPos;
+            }
+        }
+    }
+
+    public void pop()
+    {
+        if (popState == 0)
+        {
+            popPos.y += popHeight;
+            popState = 1;
+        }
+    }
+}
